ee8b7a679c70b1ebc00d9eda97875fb4cf852c85
rgupta05 <rajat.g@mindfiresolutions.com>
2021-12-10 17:37:04 +0530

    updates

--- main.go
+++ main.go
@@ -14,13 +14,9 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
-	"os"
-	"os/signal"
 	"reflect"
 	"regexp"
-	"strconv"
 	"strings"
-	"syscall"
 	"time"
 
 	"github.com/tidwall/gjson"
@@ -47,14 +43,14 @@ func main() {
 	database.Connection()
 	StreamData()
 
-	// trap sigterm or interupt and gracefully shutdown the server
-	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt)
-	signal.Notify(c, syscall.SIGTERM)
+	// // trap sigterm or interupt and gracefully shutdown the server
+	// c := make(chan os.Signal, 1)
+	// signal.Notify(c, os.Interrupt)
+	// signal.Notify(c, syscall.SIGTERM)
 
-	// Block until a signal is received.
-	sig := <-c
-	log.Println("Got signal:", sig)
+	// // Block until a signal is received.
+	// sig := <-c
+	// log.Println("Got signal:", sig)
 
 }
 
@@ -66,16 +62,16 @@ func StreamData() {
 	var count = 0
 
 	//infinite recursive call to continue stream after stopping
-	defer StreamData()
+	//defer StreamData()
 
-	authToken, err := RefreshToken()
-	if err != nil {
-		errResult := fmt.Errorf("run: %s", err)
-		log.Println(errResult)
-	}
+	// authToken, err := RefreshToken()
+	// if err != nil {
+	// 	errResult := fmt.Errorf("run: %s", err)
+	// 	log.Println(errResult)
+	// }
 
 	s.oauth2Token = &oauth2.Token{
-		AccessToken: authToken.AccessToken,
+		AccessToken: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOlsiaHR0cHM6Ly9kZnVzZS5lb3NuYXRpb24uaW8vIl0sImV4cCI6MTYzOTIyMjM1MiwiaWF0IjoxNjM5MTM1OTUyLCJpc3MiOiJodHRwczovL2FwaS5kZnVzZS5lb3NuYXRpb24uaW8vdjEvIiwic3ViIjoidWlkOjIzMzI0MTIuZW9zbiIsImFwaV9rZXlfaWQiOiJkZWZhdWx0IiwicXVvdGEiOjEyMCwicmF0ZSI6MTAsIm5ldHdvcmtzIjpbeyJuYW1lIjoiZW9zIiwicXVvdGEiOjEyMCwicmF0ZSI6MTB9LHsibmFtZSI6IndheCIsInF1b3RhIjoxMjAsInJhdGUiOjEwfSx7Im5hbWUiOiJreWxpbiIsInF1b3RhIjoxMjAsInJhdGUiOjEwfSx7Im5hbWUiOiJqdW5nbGUiLCJxdW90YSI6MTIwLCJyYXRlIjoxMH0seyJuYW1lIjoid2F4dGVzdCIsInF1b3RhIjoxMjAsInJhdGUiOjEwfSx7Im5hbWUiOiJvcmUiLCJxdW90YSI6MTIwLCJyYXRlIjoxMH0seyJuYW1lIjoib3Jlc3RhZ2UiLCJxdW90YSI6MTIwLCJyYXRlIjoxMH0seyJuYW1lIjoidGVzdG5ldCIsInF1b3RhIjoxMjAsInJhdGUiOjEwfV19.msrTcMs9tkuSsieSEAMBEDaYnMeG3jVodDNgLsECkk8",
 		TokenType:   "Bearer",
 	}
 
@@ -141,11 +137,11 @@ func StreamData() {
 	fmt.Println("LAST SEEN CURSOR  ID ::::::::", lastSeenCursor)
 
 	search := "receiver:xegkypcmeirg"
-	cursor := ""
+	cursor := "Tb9oi-JPt8HkWy0t2LjRJ_e7JZY6DlNsVArhIBtAgoij9CeQ3sz0AjQ="
 	fmt.Println(search)
-	low, _ := strconv.Atoi(lowBlockNum)
+	//low, _ := strconv.Atoi(lowBlockNum)
 	//limit, _ := strconv.Atoi(limitBlock)
-	vars := toVariable(search, cursor, int64(low), 0)
+	vars := toVariable(search, cursor, 0)
 
 	executionClient, err := graphqlClient.Execute(ctx, &graphql.Request{Query: queryTemplate, Variables: vars})
 
@@ -228,7 +224,7 @@ func StreamData() {
 		fmt.Println("BLOCK ID:", blockid)
 		fmt.Println("TRACE ID:", traceid)
 		fmt.Println("IRREVERSIBLE ID:", irreversible)
-		//	s.storage.StoreCursor(cursor)
+		//s.storage.StoreCursor(cursor)
 
 		actions := models.Cursor{
 			Cursorid:       cursor,
@@ -402,7 +398,7 @@ func ParseJwt(token string) (*JWT, error) {
 
 }
 
-func toVariable(query string, cursor string, lowBlockNum int64, limit int64) *structpb.Struct {
+func toVariable(query string, cursor string, limit int64) *structpb.Struct {
 	return &structpb.Struct{
 		Fields: map[string]*structpb.Value{
 			"search": {
@@ -413,11 +409,6 @@ func toVariable(query string, cursor string, lowBlockNum int64, limit int64) *st
 			"cursor": {
 				Kind: &structpb.Value_StringValue{
 					StringValue: cursor,
-				},
-			},
-			"lowBlockNum": {
-				Kind: &structpb.Value_NumberValue{
-					NumberValue: float64(lowBlockNum),
 				},
 			},
 			"limit": {
